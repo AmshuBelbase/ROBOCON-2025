311670d7d7280fb20d85ffab9dbdaba75aa8c448
ab0667 <ab0667@srmist.edu.in>
2025-06-07 06:59:15 +0530

    on test

--- R1/TargetLocking/test.py
+++ R1/TargetLocking/test.py
@@ -5,7 +5,9 @@ import cv2
 import serial
 import time
 
-teensy_port = "/dev/tty.usbmodem166413901"  
+# on test
+
+teensy_port = "/dev/tty.usbmodem166413901"
 baud_rate = 2000000
 
 with serial.Serial(teensy_port, baud_rate, timeout=1) as ser:
